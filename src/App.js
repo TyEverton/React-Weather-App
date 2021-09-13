3577084d6c061e7d3173804f5e13101ab550d6e9
Ty Everton <everton_915@yahoo.com>
2021-09-13 17:41:19 -0600

    build search bar and linked the weather api from api.openweathermap.org

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react"
+import React from 'react'
+
+const api = {
+  key: 'ab713336d4828b653c66db543e48f445',
+  base: 'https://api.openweathermap.org/data/2.5/',
+}
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+        <div className="searchBox">
+          <input type="text" className="searchBar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
