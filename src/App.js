066a82a8de597833e17fec21cf497dbd5d0e0fd9
Ty Everton <everton_915@yahoo.com>
2021-09-14 20:42:25 -0600

    changed f to c for celsius. will need to work on having the temp display as fahrenheit

--- src/App.js
+++ src/App.js
@@ -74,8 +74,8 @@ function App() {
               </div>
               <div className="date">{dateBuilder(new Date())}</div>
               <div className="weatherBox">
-                <div className="temp">{Math.round(weather.main.temp)}°f</div>
-                <div className="weather">Sunny</div>
+                <div className="temp">{Math.round(weather.main.temp)}°c</div>
+                <div className="weather">{weather.weather[0].main}</div>
               </div>
             </div>
           </div>
